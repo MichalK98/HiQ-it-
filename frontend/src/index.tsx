979f385bf312b1f37777461986d7893f4a67baab
MichalK98 <michal10203040@gmail.com>
2023-02-06 21:01:02 +0100

    feat: implement styled-components & global style to the frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(<React.StrictMode>HiQ it!</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    HiQ it!
+    <GlobalStyle />
+  </React.StrictMode>
+);
