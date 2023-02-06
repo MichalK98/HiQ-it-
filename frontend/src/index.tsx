ed10efdb4551f9097a821b521d3f4607851deb0f
MichalK98 <michal10203040@gmail.com>
2023-02-06 21:05:17 +0100

    feat: Implement react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
+import { Home, NotFound } from './pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+]);
 
 root.render(
   <React.StrictMode>
     HiQ it!
     <GlobalStyle />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
